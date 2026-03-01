7e2f0cfe2aff17161f22c611347b60a6250d18da
Ilias Hirschmann <253711797+IliHirsch@users.noreply.github.com>
2026-03-01 13:34:33 +0100

    windows app created

---
+++ electron.cjs
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+app.setLoginItemSettings({
+  openAtLogin: true,
+})
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    backgroundColor: '#080810',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  })
+
+  win.loadFile(path.join(__dirname, 'dist/index.html'))
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
